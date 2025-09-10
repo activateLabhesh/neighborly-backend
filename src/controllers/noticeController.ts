335c344e9c6e757ddbcea8a6a66d7b00f34c46d6
Labhesh <labhesh.2024@vitstudent.ac.in>
2025-09-10 18:41:22 +0530

    polls and notices imporved and tested

--- src/controllers/noticeController.ts
+++ src/controllers/noticeController.ts
@@ -2,10 +2,9 @@ import  supabase  from "../config/supabase";
 import { Response, Request } from "express";
 
 
-
 export const createnotice = async (req: Request, res: Response) => {
     try {
-        const { id: userId } = req.params as { id?: string }; // change to req.body later for admin..
+        const userId = req.user?.id; 
         const { title, content } = req.body as { title?: string, content?: string  };
 
         if (!userId) {
@@ -16,7 +15,6 @@ export const createnotice = async (req: Request, res: Response) => {
         }
         
         const { data, error } = await supabase
-
             .from("notices")
             .insert({ 'title':title, 
                 'content': content })
@@ -35,7 +33,7 @@ export const createnotice = async (req: Request, res: Response) => {
 
 export const editnotice = async (req: Request, res: Response) => {
     try {
-        const { id: userId } = req.params as { id?: string }; // change to req.body later for admin..
+        const userId = req.user?.id; 
         const { noticeId, title, content } = req.body as { noticeId?: string, title?: string, content?: string  };
         
         if (!userId) {
@@ -68,7 +66,7 @@ export const editnotice = async (req: Request, res: Response) => {
 
 export const deletenotice = async (req: Request, res: Response) => {
     try {
-        const { id: userId } = req.params as { id?: string }; // change to req.body later for admin..
+        const userId = req.user?.id; 
         const { noticeId } = req.body as { noticeId?: string  };
 
         if (!userId) {
@@ -88,6 +86,30 @@ export const deletenotice = async (req: Request, res: Response) => {
             return res.status(400).json({ message: error.message });
         }
         return res.status(200).json({ message: "Notice deleted successfully", data });  
+    }
+    catch(error){
+        const errorMessage = error instanceof Error ? error.message : String(error);
+        return res.status(500).json({message: errorMessage});
+    }
+};
+
+export const fetchnotices = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user?.id; 
+
+        if (!userId) {
+            return res.status(400).json({ message: "Missing requesting user id in route params (:id)" });
+        }
+
+        const { data, error } = await supabase
+            .from("notices")
+            .select("title,content")
+            .order('created_at', { ascending: false });
+
+        if (error) {
+            return res.status(400).json({ message: error.message });
+        }  
+        return res.status(200).json({ message: "Notices fetched successfully", data });
     }
     catch(error){
         const errorMessage = error instanceof Error ? error.message : String(error);
